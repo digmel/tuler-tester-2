58f3a3ac180178066e9a77d30bbf7cc654b91866
digmel <47932883+digmel@users.noreply.github.com>
2023-01-28 23:48:52 +0000

    Deploying to public from @ digmel/tuler-tester-2@64d9deb7de8703dc76228552a246e97cec361f3c 🚀

---
+++ precache-manifest.ae049143d91bcfa9f9172f42289e2aea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4836e498fa9e7d9f12acb283e6055f5f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1e00ab8b2ac0bc4f8bda",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5827e04b23f25360549b",
+    "url": "./static/js/2.2eb27c04.chunk.js"
+  },
+  {
+    "revision": "e014ead73f636818c4254719a1ba0cb8",
+    "url": "./static/js/2.2eb27c04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e00ab8b2ac0bc4f8bda",
+    "url": "./static/js/main.fdb1dde6.chunk.js"
+  },
+  {
+    "revision": "3c17855bf561b12bd7e8",
+    "url": "./static/js/runtime-main.249d128d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0c9d4907f9f6d9a8e7ddfce2f63563ca.js"
+  "./precache-manifest.ae049143d91bcfa9f9172f42289e2aea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
