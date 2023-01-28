b17b5a89a458ba46244628134b32818fe2d7f2e4
George <gio.digmel@gmail.com>
2023-01-28 17:56:05 -0600

    fix: backend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // Import dependencies
 import React, { useRef, useEffect } from "react";
-// import * as tf from "@tensorflow/tfjs";
+// eslint-disable-next-line no-unused-vars
+import * as tf from "@tensorflow/tfjs";
 import * as cocossd from "@tensorflow-models/coco-ssd";
 import Webcam from "react-webcam";
 import "./App.css";
